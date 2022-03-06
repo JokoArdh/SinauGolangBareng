577f6134ab895d336da085baa5a4954913beae26
JokoArdh <jokoardianto12@gmail.com>
2022-03-06 18:01:20 +0700

    intro program awalan

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main //program awal harus di inisialisasian sebagai header main
+
+import "fmt" //cara memasukan library standart atau bawaan
+
+//dalam golang hanya mengenal sebuah function jadi setiap
+//di go sama dengan python tidak menggunakan ; u akhir an
+//membuat sesuatu harus disertai fuuncionnya
+//func main adalah function defaulnya dalam awalan ini kita akan mengenal
+//func ini dahulu
+func main() {
+	fmt.Println("Hellow Word, selamat belajar bahasa golang") // diawali dengan fmt ibarat sebuat library standart bawaan go
+}
