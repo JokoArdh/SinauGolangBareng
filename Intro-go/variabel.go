d75fc0b4cffe69a899fa9cd20fd4b2114380bfe2
JokoArdh <jokoardianto12@gmail.com>
2022-03-08 21:12:26 +0700

    cara membuat variabel

---
+++ Intro-go/variabel.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//penulisan variabel dengan metode VAR
+	var text = "halo semua"
+	//menampilkan
+	fmt.Println(text)
+
+	//penulisan gaya dengan metode tipe data
+	var kata string
+	kata = "sedang belajar GO"
+	fmt.Println(kata)
+
+	//dengan metode :=
+	prodi := "Teknik informatika"
+	fmt.Println(prodi)
+
+}
